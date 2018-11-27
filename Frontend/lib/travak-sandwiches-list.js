a705938e9a0ae53425cd6bd5404ecaf6cd9edc9c
Tom L <lemmens.tom11@gmail.com>
2018-11-27 11:38:02 +0100

    Correcte link

--- Frontend/lib/travak-sandwiches-list.js
+++ Frontend/lib/travak-sandwiches-list.js
@@ -4,7 +4,7 @@ class DenTravakSandwichesList extends DenTravakAbstractElement {
 
     connectedCallback() {
         super.connectedCallback();
-        fetch('/api/sandwiches.json')
+        fetch('localhost:8080/sandwiches')
             .then(resp => resp.json())
             .then(json => this.updateSandwichesList(json));
     }
