df9ce58ec9892c7344689f70c307af211aa2d888
Tom L <lemmens.tom11@gmail.com>
2018-11-27 10:01:33 +0100

    Tests - werkt nog niet

--- src/test/java/be/ucll/da/dentravak/controllers/SandwichControllerIntegrationTest.java
+++ src/test/java/be/ucll/da/dentravak/controllers/SandwichControllerIntegrationTest.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
+
 import static be.ucll.da.dentravak.model.SandwichTestBuilder.aSandwich;
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 
@@ -47,9 +49,16 @@ public class SandwichControllerIntegrationTest extends AbstractControllerIntegra
     @Test
     public void testPutSandwich() throws JSONException {
        // throw new RuntimeException("Implement this test and then the production code");
-        Sandwich sandwich = aSandwich().withName("Americain").withIngredients("Vlees").withPrice(4.0).build();
-        String expectedSandwichAsJson = "{\"id\":\"${json-unit.ignore}\",\"name\":\"Americain\",\"ingredients\":\"Vlees\",\"price\":4}";
+        Sandwich sandwich = aSandwich().withName("Testkaas").withIngredients("Vlees").withPrice(4.0).build();
+        String expectedSandwichAsJson = "{\"id\":\"${json-unit.ignore}\",\"name\":\"Testkaas\",\"ingredients\":\"Vlees\",\"price\":4}";
         String actualSandwichAsJson = httpPost("/sandwiches", sandwich);
+        assertThatJson(actualSandwichAsJson).isEqualTo(expectedSandwichAsJson);
+//now updates
+        sandwich = aSandwich().withName("Testkaas").withIngredients("Vlees").withPrice(5.0).build();
+        actualSandwichAsJson = httpPut("/sandwiches", sandwich);
+
+        expectedSandwichAsJson = "{\"id\":\"${json-unit.ignore}\",\"name\":\"Testkaas\",\"ingredients\":\"Vlees\",\"price\":5}";
+
         assertThatJson(actualSandwichAsJson).isEqualTo(expectedSandwichAsJson);
 
     }
