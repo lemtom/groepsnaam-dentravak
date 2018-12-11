9257ce1c9917ebde81b9b3b1fbec6a31db361820
Tom L <lemmens.tom11@gmail.com>
2018-12-11 09:45:08 +0100

    Dingskes van de source github

--- src/main/java/be/ucll/da/dentravak/Application.java
+++ src/main/java/be/ucll/da/dentravak/Application.java
@@ -5,13 +5,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
 
     @Configuration
     public class WebConfig implements WebMvcConfigurer {
