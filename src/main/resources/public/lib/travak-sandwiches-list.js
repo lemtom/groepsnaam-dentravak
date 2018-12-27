de0224073e2c8e3a2814fc856384b9e559d5c2ae
Tom L <lemtom@users.noreply.github.com>
2018-12-27 16:07:31 +0100

    Relative url

--- src/main/resources/public/lib/travak-sandwiches-list.js
+++ src/main/resources/public/lib/travak-sandwiches-list.js
@@ -4,7 +4,7 @@ class DenTravakSandwichesList extends DenTravakAbstractElement {
 
     connectedCallback() {
         super.connectedCallback();
-        fetch('http://localhost:8080/den-travak/sandwiches')
+        fetch('/den-travak/sandwiches')
             .then(resp => resp.json())
             .then(json => this.updateSandwichesList(json));
     }
